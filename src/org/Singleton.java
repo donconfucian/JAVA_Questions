d5f05ae130d131f3b951f99631e381846f7e12e1
donconfucian <donconfucian@hotmail.com>
2021-06-15 14:14:49 +0800

    Initial commit

---
+++ src/org/Singleton.java
@@ -0,0 +1,16 @@
+package org;
+
+/**
+ * common initialization
+ */
+
+public class Singleton {
+    private Singleton() {
+    }
+    private static final Singleton instance = new Singleton();
+
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+}
---
+++ src/org/Singleton_Test.java
@@ -0,0 +1,11 @@
+package org;
+
+public class Singleton_Test {
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1==s2);
+
+    }
+}
