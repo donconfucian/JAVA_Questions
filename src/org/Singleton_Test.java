ae32e1a71fb82510950589619563c39bc7c4b2b9
donconfucian <donconfucian@hotmail.com>
2021-06-15 14:21:36 +0800

    Add multithreading test for Singleton Pattern

--- src/org/Singleton_Test.java
+++ src/org/Singleton_Test.java
@@ -1,11 +1,24 @@
 package org;
 
+import javax.swing.plaf.TableHeaderUI;
+
 public class Singleton_Test {
 
     public static void main(String[] args) {
+        //test case 1
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
         System.out.println(s1==s2);
+
+        //test case 2
+        for (int i = 0; i < 20; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton.getInstance());
+                }
+            }).start();
+        }
 
     }
 }
